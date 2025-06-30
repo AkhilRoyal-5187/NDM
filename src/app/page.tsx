5bbd3bcf3226e5fe3026d06c3a135f096c077043
AkhilRoyal-5187 <akhilroyal5187@gmail.com>
2025-06-30 16:25:06 +0530

    i to i : number error fix in the versal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ const iconItems = [
 const HomePage = () => {
   const leftVariants = {
     hidden: { opacity: 0, y: 30 },
-    visible: (i) => ({
+    visible: (i: number) => ({
       opacity: 1,
       y: 0,
       transition: { delay: i * 0.2, duration: 0.6 },
